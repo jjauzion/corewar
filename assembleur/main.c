fb6d6b02c31038a2f55c0769dc4a88a65f59d0cc
smortier42 <smortier@e1r1p14.42.fr>
2018-06-03 18:55:30 +0200

    GOOD LUCK

--- assembleur/main.c
+++ assembleur/main.c
@@ -1,7 +1,45 @@
 #include "asm.h"
 #include "libft.h"
 
-int		main (void)
+int		main (int ac, char **av)
 {
-		ft_printf("YOLO %d\n", 42);
+	int		fd;
+	char	*line;
+	int		index;
+	int		index2;
+	t_params params;
+
+	if (ac != 2 || !ft_strstr(av[1], ".s"))
+	{
+		ft_printf("usage : ./asm file.s\n");
+		return (0);
+	}
+	if ((fd = open(av[1], O_RDONLY)) == -1)
+	{
+		ft_printf("This file does not exist !\n");
+		return (0);
+	}
+	index = 0;
+	while (get_next_line(fd, &line) == 1)
+	{
+		if (line[0] && line[0] != '#')
+			index += 1;
+		ft_memdel((void **)&line);
+	}
+	ft_memdel((void **)&line);
+	lseek(fd, 0, SEEK_SET);
+	if (!(params.file = (char **)ft_memalloc(sizeof(char *) * index + 1)))
+		return (0);
+	params.file[index] = NULL;
+	index2 = -1;
+	while (get_next_line(fd, &line) == 1)
+	{
+		if (line[0] && line[0] != '#')
+			params.file[++index2] = line;
+		else
+			ft_memdel((void **)&line);
+	}
+	ft_memdel((void **)&line);
+	close(fd);
+	lexer(&params);
 }
