cb4be7a9488e34cd02fbd8265c5c60695c77e1aa
jjauzion <jjauzion@student.42.fr>
2018-06-04 13:47:04 +0200

    read_champ, libft/print_mem modif

--- libft/ft_print_mem.c
+++ libft/ft_print_mem.c
@@ -6,7 +6,7 @@
 /*   By: jjauzion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 12:02:58 by jjauzion          #+#    #+#             */
-/*   Updated: 2018/05/28 12:04:14 by jjauzion         ###   ########.fr       */
+/*   Updated: 2018/06/04 10:16:52 by jjauzion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,5 @@ void		ft_print_mem(void *adr, size_t length)
 		ft_printf("%02X", (unsigned)byte_array[i]);
 		i++;
 	}
+	ft_printf("\n");
 }
--- vm/read_champ.c
+++ vm/read_champ.c
@@ -6,7 +6,7 @@
 /*   By: jjauzion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/03 17:25:23 by jjauzion          #+#    #+#             */
-/*   Updated: 2018/06/03 19:09:15 by jjauzion         ###   ########.fr       */
+/*   Updated: 2018/06/04 12:31:22 by jjauzion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,13 @@ t_champion		*read_champ(int argc, char **argv, int nb_champ)
 	char		buff[CHAMP_MAX_SIZE + PROG_NAME_LENGTH + COMMENT_LENGTH];
 	t_champion	*champion;
 	int			tmp;
+	int			ret;
+	void		*ptr;
+	int			nb_byte;
 
 	if (!(champion = (t_champion*)ft_memalloc(sizeof(t_champion) * nb_champ)))
 		return (NULL);
+	nb_byte = sizeof(unsigned int);
 	i = 0;
 	while (++i < argc)
 	{
@@ -30,11 +34,38 @@ t_champion		*read_champ(int argc, char **argv, int nb_champ)
 		{
 			if ((fd = open(argv[i], O_RDONLY)) == -1)
 				return (error_ptr("File not found"));
-			if (read(fd, buff, CHAMP_MAX_SIZE + PROG_NAME_LENGTH + COMMENT_LENGTH) == 0)
+			if ((ret = read(fd, buff, CHAMP_MAX_SIZE + PROG_NAME_LENGTH + COMMENT_LENGTH + 1)) > 
+					CHAMP_MAX_SIZE + PROG_NAME_LENGTH + COMMENT_LENGTH)
 				return (error_ptr("Champion is too big"));
+			close(fd);
+			ft_printf("ret = %d\n", ret);
+			ft_print_mem((void*)buff, ret);
+			ptr = &champion->header.magic;
 			j = -1;
-			tmp = (int)*buff;
-			ft_printf("tmp = %d\n", tmp);
+			while (++j < nb_byte)
+				ft_memset(&ptr[nb_byte - 1 - j], (int)buff[j], 1);
+			ft_printf("magic = %u\n", champion->header.magic);
+			while (buff[j] != 0)
+			{
+				champion->header.prog_name[j - nb_byte] = buff[j];
+				if (j - nb_byte > PROG_NAME_LENGTH)
+					return (error_ptr("Programme name is too long\n"));
+				j++;
+			}
+			ft_printf("%s\n", champion->header.prog_name);
+			ptr = &champion->header.prog_size;
+			j = -1;
+			while (++j < nb_byte)
+				ft_memset(&ptr[nb_byte - 1 - j], (int)buff[j + nb_byte + PROG_NAME_LENGTH + 1 + 2 + 1], 1);
+			ft_printf("prog_size = %u\n", champion->header.prog_size);
+			j = -1;
+			while (buff[++j + nb_byte * 2 + PROG_NAME_LENGTH + 1 + 2 + 1] != 0)
+			{
+				champion->header.comment[j] = buff[j + nb_byte * 2 + PROG_NAME_LENGTH + 1 + 2 + 1];
+				if (j > COMMENT_LENGTH)
+					return (error_ptr("Programme comment is too long\n"));
+			}
+			ft_printf("%s\n", champion->header.comment);
 		}
 	}
 	return (NULL);
