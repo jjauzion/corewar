715b2c9da55df8627ed09bd672e70b54e80e5bfd
Simon PLIESEIS <s.plieseis@hotmail.com>
2018-06-08 14:28:35 +0200

    first few functions for asm

--- assembleur/check_params.c
+++ assembleur/check_params.c
@@ -6,15 +6,59 @@
 /*   By: spliesei <spliesei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 17:31:50 by spliesei          #+#    #+#             */
-/*   Updated: 2018/06/07 17:35:39 by spliesei         ###   ########.fr       */
+/*   Updated: 2018/06/08 14:20:21 by spliesei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
+int	check_comment(char *line)
+{
+	line = skip_whitespace(line);
+	if (line[0] == COMMENT_CHAR)
+		return (1);
+	return (0);
+}
+
+int	check_label_chars(char *line, int i)
+{
+	while (--i >= 0)
+	{
+		if (ft_strchr(LABEL_CHARS, line[i]) == NULL)
+			return (0);
+	}
+	return (1);
+}
+
+int	check_label(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] && line[i] != LABEL_CHAR)
+		i++;
+	if (line[i])
+	{
+		if (check_label_chars(line, i))
+			return (1);
+	}
+	return (0);
+}
+
 int	check_live_par(char *line)
 {
+	int	i;
 
+	i = 0;
+	line = skip_whitespace(line);
+	if (line[i] == DIRECT_CHAR)
+	{
+		if (check_int(&line[i + 1])) // Checks if the Number in the string is an int.
+			return (1);
+		else
+			error_live(line);
+	}
+	return (0);
 }
 
 int	check_ld_par(char *line)
@@ -84,7 +128,7 @@ int	check_lldi_par(char *line)
 
 int	check_lfork_par(char *line)
 {
-	
+
 }
 
 int	check_aff_par(char *line)
