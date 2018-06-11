17a99bb24815e8dd97a9ae16de9491297e07de16
smortier42 <smortier@e3r1p11.42.fr>
2018-06-11 15:21:58 +0200

    here we go

--- assembleur/analyze_line.c
+++ assembleur/analyze_line.c
@@ -22,43 +22,41 @@ void	analyze_line(t_params *params, char *line, int index_line)
 	// ft_printf("secon word: %s\n", split[1]);
 
 	(void)params;
+
+
+	else if (ft_strncmp("add", split[0], 3) == 0 && check_add_par(line) == 1)
+		;// save_add(params, line);
+	else if (ft_strncmp("sub", split[0], 3) == 0 && check_sub_par(line) == 1)
+		;// save_sub(params, line);
+	else if (ft_strncmp("and", split[0], 3) == 0 && check_and_par(line) == 1)
+		;// save_and(params, line);
+	else if (ft_strncmp("or", split[0], 2) == 0 && check_or_par(line) == 1)
+		;// save_or(params, line);
+	else if (ft_strncmp("xor", split[0], 3) == 0 && check_xor_par(line) == 1)
+		;// save_xor(params, line);
+	else if (ft_strncmp("zjmp", split[0], 4) == 0 && check_zjmp_par(line) == 1)
+			;// save_zjmp(params, line);
+	else if (ft_strncmp("ldi", split[0], 3) == 0 && check_ldi_par(line) == 1)
+			;//save_ldi(params, line);
+	else if (ft_strncmp("sti", split[0], 3) == 0 && check_sti_par(line) == 1)
+			;// save_sti(params, line);
+	else if (ft_strncmp("fork", split[0], 4) == 0 && check_fork_par(params, line+ 4, index_line))
+			;// save_fork(params, line);
+	else if (ft_strncmp("lldi", split[0], 4) == 0 && check_lldi_par(params, line + 4, index_line))
+			;// save_lldi(params, line);
+	else if (ft_strncmp("lld", split[0], 3) == 0 && check_lld_par(params, line + 3, index_line))
+			;// save_lld(params, line);
+	else if (ft_strncmp("lfork", split[0], 5) == 0 && check_lfork_par(params, line + 5, index_line))
+			; // save_lfork(params, line);
+	else if (ft_strncmp("aff", split[0], 3) == 0 && check_aff_par(params, line + 3, index_line))
+			; // save_aff(params, line);
 	if (ft_strncmp("live", split[0], 4) == 0 && check_live_par(params, line + 4,
 			index_line))
-		// save_live(params, line + 4);
-		;
+			;	// save_live(params, line + 4);
 	else if (ft_strncmp("ld", split[0], 2) == 0 && check_ld_par(params, line + 2, index_line))
-		;
-	// 	save_ld(params, line);
-	// else if (ft_strncmp("st", split[0], 2) == 0 && check_st_par(line) == 1)
-	// 	save_st(params, line);
-	// else if (ft_strncmp("add", split[0], 3) == 0 && check_add_par(line) == 1)
-	// 	save_add(params, line);
-	// else if (ft_strncmp("sub", split[0], 3) == 0 && check_sub_par(line) == 1)
-	// 	save_sub(params, line);
-	// else if (ft_strncmp("and", split[0], 3) == 0 && check_and_par(line) == 1)
-	// 	save_and(params, line);
-	// else if (ft_strncmp("or", split[0], 2) == 0 && check_or_par(line) == 1)
-	// 	save_or(params, line);
-	// else if (ft_strncmp("xor", split[0], 3) == 0 && check_xor_par(line) == 1)
-	// 	save_xor(params, line);
-	// else if (ft_strncmp("zjmp", split[0], 4) == 0 && check_zjmp_par(line) == 1)
-	// 	save_zjmp(params, line);
-	// else if (ft_strncmp("ldi", split[0], 3) == 0 && check_ldi_par(line) == 1)
-	// 	save_ldi(params, line);
-	// else if (ft_strncmp("sti", split[0], 3) == 0 && check_sti_par(line) == 1)
-	// 	save_sti(params, line);
-	else if (ft_strncmp("fork", split[0], 4) == 0 && check_fork_par(params, line+ 4, index_line))
-			;// 	save_fork(params, line);
-	else if (ft_strncmp("lldi", split[0], 4) == 0 && check_lldi_par(params, line + 4, index_line))
-			;// 	save_lldi(params, line);
-	else if (ft_strncmp("lld", split[0], 3) == 0 && check_lld_par(params, line + 3, index_line))
-			;// 	save_lld(params, line);
-	else if (ft_strncmp("lfork", split[0], 5) == 0 && check_lfork_par(params, line + 5, index_line))
-			;// 	save_lfork(params, line);
-	else if (ft_strncmp("aff", split[0], 3) == 0 && check_aff_par(params, line + 3, index_line))
-			; //save_aff(params, line);
-	// else
-	// 	ft_error(line);
+			; // save_ld(params, line);
+	else if (ft_strncmp("st", split[0], 2) == 0 && check_st_par(line) == 1)
+			; //save_st(params, line);
 	index = -1;
 	while (split[++index])
 		ft_strdel(&split[index]);
