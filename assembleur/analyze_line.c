9c46a07c79f90f8fa1c3472a73c5db3a4b0ce2f2
smortier42 <smortier@e3r1p11.42.fr>
2018-06-11 15:32:56 +0200

    ok

--- assembleur/analyze_line.c
+++ assembleur/analyze_line.c
@@ -24,23 +24,23 @@ void	analyze_line(t_params *params, char *line, int index_line)
 	(void)params;
 
 
-	else if (ft_strncmp("add", split[0], 3) == 0 && check_add_par(line) == 1)
+	if (ft_strncmp("add", split[0], 3) == 0 && check_add_par(params, line + 3, index_line))
 		;// save_add(params, line);
-	else if (ft_strncmp("sub", split[0], 3) == 0 && check_sub_par(line) == 1)
+	else if (ft_strncmp("sub", split[0], 3) == 0 && check_sub_par(params, line + 3, index_line))
 		;// save_sub(params, line);
-	else if (ft_strncmp("and", split[0], 3) == 0 && check_and_par(line) == 1)
+	else if (ft_strncmp("and", split[0], 3) == 0 && check_and_par(params, line + 3, index_line))
 		;// save_and(params, line);
-	else if (ft_strncmp("or", split[0], 2) == 0 && check_or_par(line) == 1)
+	else if (ft_strncmp("or", split[0], 2) == 0 && check_or_par(params, line + 2, index_line))
 		;// save_or(params, line);
-	else if (ft_strncmp("xor", split[0], 3) == 0 && check_xor_par(line) == 1)
+	else if (ft_strncmp("xor", split[0], 3) == 0 && check_xor_par(params, line + 3, index_line))
 		;// save_xor(params, line);
-	else if (ft_strncmp("zjmp", split[0], 4) == 0 && check_zjmp_par(line) == 1)
+	else if (ft_strncmp("zjmp", split[0], 4) == 0 && check_zjmp_par(params, line + 4, index_line))
 			;// save_zjmp(params, line);
-	else if (ft_strncmp("ldi", split[0], 3) == 0 && check_ldi_par(line) == 1)
+	else if (ft_strncmp("ldi", split[0], 3) == 0 && check_ldi_par(params, line + 3, index_line))
 			;//save_ldi(params, line);
-	else if (ft_strncmp("sti", split[0], 3) == 0 && check_sti_par(line) == 1)
+	else if (ft_strncmp("sti", split[0], 3) == 0 && check_sti_par(params, line + 3, index_line))
 			;// save_sti(params, line);
-	else if (ft_strncmp("fork", split[0], 4) == 0 && check_fork_par(params, line+ 4, index_line))
+	else if (ft_strncmp("fork", split[0], 4) == 0 && check_fork_par(params, line + 4, index_line))
 			;// save_fork(params, line);
 	else if (ft_strncmp("lldi", split[0], 4) == 0 && check_lldi_par(params, line + 4, index_line))
 			;// save_lldi(params, line);
@@ -50,12 +50,12 @@ void	analyze_line(t_params *params, char *line, int index_line)
 			; // save_lfork(params, line);
 	else if (ft_strncmp("aff", split[0], 3) == 0 && check_aff_par(params, line + 3, index_line))
 			; // save_aff(params, line);
-	if (ft_strncmp("live", split[0], 4) == 0 && check_live_par(params, line + 4,
+	else if (ft_strncmp("live", split[0], 4) == 0 && check_live_par(params, line + 4,
 			index_line))
 			;	// save_live(params, line + 4);
 	else if (ft_strncmp("ld", split[0], 2) == 0 && check_ld_par(params, line + 2, index_line))
 			; // save_ld(params, line);
-	else if (ft_strncmp("st", split[0], 2) == 0 && check_st_par(line) == 1)
+	else if (ft_strncmp("st", split[0], 2) == 0 && check_st_par(params, line + 2, index_line))
 			; //save_st(params, line);
 	index = -1;
 	while (split[++index])
--- assembleur/check_sti.c
+++ assembleur/check_sti.c
@@ -12,7 +12,51 @@
 
 #include "asm.h"
 
-int	check_sti_par(char *line)
+void check_nbr_arg_sti(char **split)
 {
+	int		i;
 
+	i = -1;
+	while (split[++i])
+    ;
+	if (i != 3)
+	{
+		ft_printf("Error:\n");
+		exit (0);
+	}
+}
+
+int	check_sti_par(t_params *params, char *line, int index_line)
+{
+	char	**split;
+	char	*arg1;
+  char	*arg2;
+  char	*arg3;
+	int		index;
+
+	split = ft_strsplit(line, SEPARATOR_CHAR);
+	check_nbr_arg_sti(split);
+	arg1 = ft_strtrim(split[0]);
+  arg2 = ft_strtrim(split[1]);
+  arg3 = ft_strtrim(split[2]);
+	index = -1;
+	while (split[++index])
+		ft_strdel(&split[index]);
+	ft_memdel((void *)&split);
+	if (check_type(params, arg1) != REG_CODE)
+  {
+		ft_printf("Error : Wrong type of arg1 on instr %d (sti)\n", index_line);
+		exit(0);
+	}
+  if (!check_type(params, arg2))
+  {
+		ft_printf("Error : Wrong type of arg2 on instr %d (fork)\n", index_line);
+		exit(0);
+	}
+  if (check_type(params, arg3) != DIR_CODE && check_type(params, arg3) != IND_CODE)
+  {
+		ft_printf("Error : Wrong type of arg3 on instr %d (fork)\n", index_line);
+		exit(0);
+	}
+	return (1);
 }
