e5c13dcbce11e744e68b810b57021a72fb7c0027
Simon PLIESEIS <s.plieseis@hotmail.com>
2018-06-11 14:40:33 +0200

    merge fix

---
+++ assembleur/check_zjmp.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_zjmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spliesei <spliesei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/11 13:04:22 by spliesei          #+#    #+#             */
+/*   Updated: 2018/06/11 14:12:53 by spliesei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+
+static	void check_nbr_arg_zjmp(char **split)
+{
+	int		i;
+
+	i = -1;
+	while (split[++i])
+		;
+	if (i != 1)
+	{
+		ft_printf("Error:\n");
+		exit (0);
+	}
+}
+
+int			check_zjmp_par(t_params *params, char *line, int index_line)
+{
+	char	**split;
+	char	*arg1;
+	int		index;
+
+	split = ft_strsplit(line, SEPARATOR_CHAR);
+	check_nbr_arg_zjmp(split);
+	arg1 = ft_strtrim(split[0]);
+	index = -1;
+	while (split[++index])
+		ft_strdel(&split[index]);
+	ft_memdel((void *)&split);
+	if (check_type(params, arg1) != DIR_CODE)
+	{
+		ft_printf("Error : Wrong type of arg1 on instr %d\n", index_line);
+		exit(0);
+	}
+	ft_printf("first: [%s]\n", arg1);
+	ft_printf("arg_type 1: %d\n", check_type(params, arg1));
+	(void)index_line;
+	return (1);
+}
