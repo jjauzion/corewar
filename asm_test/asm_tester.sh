62e8cedacbbef0be0de4a04c46f13443f1d31f20
smortier42 <smortier@e3r1p10.42.fr>
2018-06-19 08:54:01 +0200

    NEW ASM TESTER WITH MORE CHAMPS AND A BETTER TEST

--- asm_test/asm_tester.sh
+++ asm_test/asm_tester.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
-OUR_PATH=~/Documents/corewar/asm_test/our_asm
-THEIR_PATH=~/Documents/corewar/asm_test/their_asm
+OUR_PATH=~/corewar/asm_test/our_asm
+THEIR_PATH=~/corewar/asm_test/their_asm
+CHAMPS_PATH=~/corewar/asm_test/champs
 
-FILES_OUR=$OUR_PATH/*.s
-for f in $FILES_OUR
+CHAMPS=$CHAMPS_PATH/*.s
+for f in $CHAMPS
 do
   # echo "Processing $f file..." >/dev/null
   ../asm $f >/dev/null
   # take action on each file. $f store current file name
 done
 
+MOVE=$CHAMPS_PATH/*.cor
+for f in $MOVE
+do
+    mv $f $OUR_PATH
+    #printf "\e[32mI move $f in $OUR_PATH\e[0m\n"
+done
+
 FILES_THEIR=$THEIR_PATH/*.s
-for f in $FILES_THEIR
+for f in $CHAMPS
 do
   # echo "Processing $f file..." >/dev/null
   ../resource/asm $f >/dev/null
   # take action on each file. $f store current file name
 done
 
+MOVE=$CHAMPS_PATH/*.cor
+for f in $MOVE
+do
+    mv $f $THEIR_PATH
+    #printf "\e[31mI move $f in $OUR_PATH\e[0m\n"
+done
+
+
 OUR_COR=$OUR_PATH/*.cor
 THEIR_COR=$THEIR_PATH/*.cor
-
-
 
 
 for f in $OUR_COR
@@ -42,3 +56,40 @@ do
 		fi
 	done
 done
+
+
+#Find files that should compile
+for f in $OUR_COR
+do
+    finded=0
+    for d in $THEIR_COR
+    do
+    name1=${f##*/}
+    name2=${d##*/}
+    if [[ $name1 == $name2 ]]; then
+        finded=1
+    fi
+    done
+    if [[ $finded == 0 ]]; then
+        printf "\e[38;5;202mKO : $name1 compile but it shouldn't\e[0m\n"
+    fi
+done
+
+#find files that shoul compile bit does not
+for f in $THEIR_COR
+do
+    finded=0
+    for d in $OUR_COR
+    do
+    name1=${f##*/}
+    name2=${d##*/}
+    if [[ $name1 == $name2 ]]; then
+        finded=1
+    fi
+    done
+    if [[ $finded == 0 ]]; then
+        printf "\e[38;5;226mKO : $name1 may compile but doesn't\e[0m\n"
+    fi
+done
+
+#printf "\e[31mKO : $(diff -r $THEIR_PATH $OUR_PATH)\e[0m\n"
---
+++ asm_test/champs/tuto.s
@@ -0,0 +1,6 @@
+.name "TUTO"
+.comment "dawdwadaw"
+
+sti r1, %:live, %1
+
+live: live %1
