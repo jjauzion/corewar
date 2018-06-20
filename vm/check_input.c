2fe70790719091a4a58f7b2742baf2b18451185f
Theo MERLI <tmerli@e3r10p21.42.fr>
2018-06-20 16:41:48 +0200

    norme check_input

--- vm/check_input.c
+++ vm/check_input.c
@@ -6,7 +6,7 @@
 /*   By: jjauzion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 14:05:19 by jjauzion          #+#    #+#             */
-/*   Updated: 2018/06/12 10:35:45 by jjauzion         ###   ########.fr       */
+/*   Updated: 2018/06/20 16:40:32 by tmerli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,25 @@ void				*usage(char *prog_name)
 	ft_printf("\t -p  \t: Show pc movement (except for jumps)\n", prog_name);
 	ft_printf("\t -l  \t: Show lives\n", prog_name);
 	return (NULL);
+}
+
+static t_champion	**add_champ(t_champion **champions, t_arena *arena, char *c)
+{
+	if (!(champions = realloc(champions,
+					sizeof(t_champion*) * (arena->nb_champion + 1))))
+		return (error_ptr(NULL, "error realloc at champion creation"));
+	if (!(champions[arena->nb_champion] = read_champ(c)))
+	{
+		while (arena->nb_champion >= 0)
+		{
+			free(champions[arena->nb_champion]);
+			(arena->nb_champion)--;
+		}
+		free(champions);
+		return (NULL);
+	}
+	(arena->nb_champion)++;
+	return (champions);
 }
 
 t_champion			**check_input(int argc, char **argv, t_arena *arena)
@@ -42,19 +61,8 @@ t_champion			**check_input(int argc, char **argv, t_arena *arena)
 	{
 		if ((ret = option(&i, argc, argv, arena->option)) == 1)
 		{
-			if (!(champions = realloc(champions, sizeof(t_champion*) * (arena->nb_champion + 1))))
-				return (error_ptr(NULL, "error realloc at champion creation"));
-			if (!(champions[arena->nb_champion] = read_champ(argv[i])))
-			{
-				while (arena->nb_champion >= 0)
-				{
-					free(champions[arena->nb_champion]);
-					(arena->nb_champion)--;
-				}
-				free(champions);
+			if (!(champions = add_champ(champions, arena, argv[i])))
 				return (NULL);
-			}
-			(arena->nb_champion)++;
 		}
 		else if (ret == ERROR)
 			return (usage(argv[0]));
