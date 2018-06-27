b70b1f17b6b40211f44537811588698571a2cfd4
smortier42 <smortier@e3r13p9.42.fr>
2018-06-27 17:06:03 +0200

    visu

--- visu/main.lua
+++ visu/main.lua
@@ -21,9 +21,14 @@ function love.keypressed(k)
    end
 end
 
+function love.quit()
+	-- io.write("Error : Wrong file\n")
+	love.event.quit()
+end
 
 function get_init_values() -- FIRST PARSING TO GET: arena_s player_name player_com
     line = io.read("*line")
+	if (line == nil) then love.quit() end
     arena_s = tonumber(string.sub(line, 14, string.len(line)))
     line = io.read("*line")
     nbr_champs = tonumber(string.sub(line, 16, string.len(line)))
@@ -49,9 +54,11 @@ function print_title()
     love.graphics.setFont(font_info)
     love.graphics.setColor(1, 0.5, 0.5, 0.7)
     love.graphics.print("By smortier, jjauzion, tmerli, spliesei", 10, lag_title)
-    love.graphics.setColor(255, 255, 255)
-	love.graphics.print("Cycle To DIE : " .. cycle_to_die, w_widht / 2 + 500, 30)
-	love.graphics.print("Number of processes : " .. index_process - 1, w_widht / 2 + 500, 30 + font_info:getHeight("CDIE"))
+	if (ending ~= 1) then
+	    love.graphics.setColor(255, 255, 255)
+		love.graphics.print("Cycle To DIE : " .. cycle_to_die, w_widht / 2 + 500, 30)
+		love.graphics.print("Number of processes : " .. index_process - 1, w_widht / 2 + 500, 30 + font_info:getHeight("CDIE"))
+	end
 end
 
 function refresh_players()
@@ -163,8 +170,8 @@ function        love.load()
     love.window.setMode(2560, 1440, {resizable=false, vsync=false, minwidth=400, minheight=300})
     success = love.window.setFullscreen(true)
     w_widht, w_height = love.graphics.getDimensions()
-    io.write(w_widht .. '\n' .. w_height .. '\n')
 	cycle = 1
+	ending = 0
 	process_tab = {}
     player_name = {}
     player_com = {}
@@ -182,74 +189,88 @@ function        love.load()
     -- line = io.read("*line")
 end
 
-function        love.update()
+function        love.update(dt)
 	if (pause == true) then return end
 	line = io.read("*line")
 	index_process = 1
 	local champ = 1
-	while (line ~= "Arena_memory :") do
+	while (line ~= "Arena_memory :" and ending ~= 1) do
 		line = io.read("*line")
 		if (string.sub(line, 1, 7) == "Contest") then
-			return
+			ending = 1
+			final_line = line
+			-- io.write(line .. '\n')
 		end
-		if (string.sub(line, 1, 7) == "process") then
-			process_tab[index_process] = tonumber(string.sub(line , 26, string.len(line)))
-			-- io.write(process_tab[index_process] .. '\n')
-			index_process = index_process + 1;
-		end
-		if (string.sub(line, 1, 7) == "cycle =") then
-			cycle = tonumber(string.sub(line, 9, string.len(line)))
-		end
-		if (string.sub(line, 1, 12) == "cycle_to_die") then
-			cycle_to_die = tonumber(string.sub(line, 16, string.len(line)))
-		end
-		if (string.sub(line, 1, 8) == "champion") then
-			player_live[champ] = tonumber(string.sub(line, 22, string.len(line)))
-			champ = champ + 1
-			io.read("*line")
+		if (ending ~= 1) then
+			if (string.sub(line, 1, 7) == "process") then
+				process_tab[index_process] = tonumber(string.sub(line , 26, string.len(line)))
+				-- io.write(process_tab[index_process] .. '\n')
+				index_process = index_process + 1;
+			end
+			if (string.sub(line, 1, 7) == "cycle =") then
+				cycle = tonumber(string.sub(line, 9, string.len(line)))
+			end
+			if (string.sub(line, 1, 12) == "cycle_to_die") then
+				cycle_to_die = tonumber(string.sub(line, 16, string.len(line)))
+			end
+			if (string.sub(line, 1, 8) == "champion") then
+				player_live[champ] = tonumber(string.sub(line, 22, string.len(line)))
+				champ = champ + 1
+				io.read("*line")
+			end
 		end
 	end
-	local index = 1
-	while (index <= 4096) do
-		line = io.read("*line")
-		for i = 1, (64 * 3) - 1, 3 do
-			data[index] = string.sub(line, i, i + 2)
-			-- io.write("data[" .. index .. "] = " .. data[index] .. '\n')
-			-- io.write(line)
-			index = index + 1
+	if (ending ~= 1) then
+		local index = 1
+		while (index <= 4096) do
+			line = io.read("*line")
+			for i = 1, (64 * 3) - 1, 3 do
+				data[index] = string.sub(line, i, i + 2)
+				-- io.write("data[" .. index .. "] = " .. data[index] .. '\n')
+				-- io.write(line)
+				index = index + 1
+			end
+			-- io.write(line .. '\n')
 		end
-		-- io.write(line .. '\n')
 	end
 	-- io.write(line .. '\n')
 end
 
 function        love.draw()
-
-    print_title()
-    love.graphics.print('Press Escape to Leave', w_widht - font_info:getWidth("Press Escape to Leave") - 10, 10)
-    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), w_widht - font_info:getWidth("Current FPS: "..tostring(love.timer.getFPS( ))) - 10, 30)
-    love.graphics.print("Time since begin : " .. round(love.timer.getTime() - start_time, 0) .. " seconds", w_widht - font_info:getWidth("Time since begin : " .. round(love.timer.getTime() - start_time, 0) .. " seconds") - 10, 70)
-	love.graphics.setFont(font_title)
-	love.graphics.print("25 BONUS POINTS", w_widht / 2 - (font_title:getWidth("25 BONUS POINTS") / 2), 10)
-	love.graphics.setFont(font_player_name)
-	love.graphics.setColor(0, 1, 0, 0.7)
-	love.graphics.print("Cycles : " .. cycle , w_widht / 2 - font_player_name:getWidth("Cycles : ") / 2, font_title:getHeight("25 BONUS POINTS") + 5)
-    -- love.graphics.print(round(love.timer.getTime() - start_time, 0), 10, 70)
-    --DRAW LES DEUX RECTANGLES
-    love.graphics.setColor(1, 1, 1)
-	if (string.sub(line, 1, 7) == "Contest") then
-		-- love.graphics.setCanvas(canvas_arena)
-		love.graphics.clear()
+	if (ending == 1) then
+		print_title()
+		-- io.write(line .. '\n')
+		love.graphics.setColor(1, 1, 1, 1)
+		love.graphics.setCanvas(canvas_arena)
+	    -- love.graphics.clear()
+	    -- love.graphics.setColor(0, 1, 0.5, 0.4)
 		love.graphics.setFont(font_title)
-		love.graphics.print(line, 0, 0)
-		love.timer.sleep(5)
-		-- love.graphics.setCanvas()
-		love.event.quit()
-		os.exit()
+		love.graphics.setColor(0, 1, 1, 1)
+	    love.graphics.rectangle('line', 0, 0, canvas_arena:getWidth(), canvas_arena:getHeight())
+	    love.graphics.setColor(1, 1, 1, 1)
+	    love.graphics.print(final_line, canvas_arena:getWidth() / 2 - font_title:getWidth(final_line) / 2, canvas_arena:getHeight() / 2)
+		love.graphics.setFont(font_player_data)
+		love.graphics.print("Press ESC to leave !\n", canvas_arena:getWidth() / 2 - font_player_data:getWidth("Press ecs to leave !\n") / 2, canvas_arena:getHeight() / 2 + font_title:getHeight() + 5)
+		love.graphics.setCanvas()
+		love.graphics.draw(canvas_arena, 30, w_height / 6 - 100)
 	end
-    refresh_arena()
-    refresh_players()
-    draw_arena()
-    draw_player()
+	if (ending ~= 1) then
+	    print_title()
+	    love.graphics.print('Press Escape to Leave', w_widht - font_info:getWidth("Press Escape to Leave") - 10, 10)
+	    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), w_widht - font_info:getWidth("Current FPS: "..tostring(love.timer.getFPS( ))) - 10, 30)
+	    love.graphics.print("Time since begin : " .. round(love.timer.getTime() - start_time, 0) .. " seconds", w_widht - font_info:getWidth("Time since begin : " .. round(love.timer.getTime() - start_time, 0) .. " seconds") - 10, 70)
+		love.graphics.setFont(font_title)
+		love.graphics.print("25 BONUS POINTS", w_widht / 2 - (font_title:getWidth("25 BONUS POINTS") / 2), 10)
+		love.graphics.setFont(font_player_name)
+		love.graphics.setColor(0, 1, 0, 0.7)
+		love.graphics.print("Cycles : " .. cycle , w_widht / 2 - font_player_name:getWidth("Cycles : ") / 2, font_title:getHeight("25 BONUS POINTS") + 5)
+	    -- love.graphics.print(round(love.timer.getTime() - start_time, 0), 10, 70)
+	    --DRAW LES DEUX RECTANGLES
+	    love.graphics.setColor(1, 1, 1)
+	    refresh_arena()
+	    refresh_players()
+	    draw_arena()
+	    draw_player()
+	end
     -- love.graphics.line(w_widht / 2 , w_height / 2, w_widht / 2, w_height)
 end
